ea3305aba2424c22692f7764ca463366e044fe4c
kk <2930293930@qq.com>
2017-10-30 11:42:58 +0800

    fix server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 var http = require('http');
 var router = require('../router/router.js');
 
-var server = http.createServer();
-server.on('request', function(req, res) {
-	router(req, res);
-});
-server.listen(3000, () => {
-	console.log('listening the port 3000');
-});
+exports = module.exports = start;
+
+function start() {
+  var server = http.createServer();
+  server.on('request', function(req, res) {
+    router(req, res);
+  });
+  server.listen(3000, () => {
+    console.log('listening the port 3000');
+  });
+}
+
+// start();
